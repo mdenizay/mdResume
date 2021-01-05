ac01c872eb033a6c61c2304ddbebd26aef5a642e
Mehmet Deniz Aydın <mdenizaydin20@gmail.com>
2021-01-05 21:29:25 +0300

    Updates maintenance mode and navbar

--- index.php
+++ index.php
@@ -7,15 +7,15 @@ session_start();
 
   if ($config_site['site_status'] == 1) {
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2);
-    //$lang = 'tr';
+    // $lang = 'en';
     if ($lang == 'tr' OR $lang == 'en' OR $lang == 'ru' OR $lang == 'ja' OR $lang == 'de' OR $lang == 'ar') {
       $lang_site = $db->query("SELECT * FROM lang_site WHERE lang = '{$lang}'")->fetch(PDO::FETCH_ASSOC);
     } else {
       $lang_site = $db->query("SELECT * FROM lang_site WHERE lang = 'tr'")->fetch(PDO::FETCH_ASSOC);
     }
-	
+
     require_once 'includes/header.php';
-	
+
     if (isset($_GET['page'])) {
       if (file_exists('pages/'.$_GET['page'].'.php')) {
         include 'pages/'.$_GET['page'].'.php';
