ddd2450d75e9d8b1e1c71abbb3061c3a202ad003
Mehmet Deniz Aydın <mdenizaydin20@gmail.com>
2021-01-05 18:53:20 +0300

    Get browser language has been added

--- index.php
+++ index.php
@@ -6,18 +6,16 @@ session_start();
   $config_site = $db->query("SELECT * FROM config_site")->fetch(PDO::FETCH_ASSOC);
 
   if ($config_site['site_status'] == 1) {
-    // $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2);
-    $lang = 'tr';
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2);
+    //$lang = 'tr';
     if ($lang == 'tr' OR $lang == 'en' OR $lang == 'ru' OR $lang == 'ja' OR $lang == 'de' OR $lang == 'ar') {
       $lang_site = $db->query("SELECT * FROM lang_site WHERE lang = '{$lang}'")->fetch(PDO::FETCH_ASSOC);
     } else {
       $lang_site = $db->query("SELECT * FROM lang_site WHERE lang = 'tr'")->fetch(PDO::FETCH_ASSOC);
     }
-
-
-
+	
     require_once 'includes/header.php';
-
+	
     if (isset($_GET['page'])) {
       if (file_exists('pages/'.$_GET['page'].'.php')) {
         include 'pages/'.$_GET['page'].'.php';
